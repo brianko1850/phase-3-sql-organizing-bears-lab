21b282449d563679f19eaf82eb201d8086fe3906
brianko <kobj11@gmail.com>
2022-11-06 21:23:10 -0800

    commit tree 18dbe5ad10d4d7229282a03285248fcb23b914ce

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+ VALUES ('Mr.Chocolate', 1, 'F', 'red', 'fucked', true),
+        ('Rowdy', 2, 'F', 'brown', 'calm', true),
+        ('Tabitha', 3, 'F', 'black', 'fucked', false),
+        ('Sergent Brown', 4, 'F', 'blue', 'fucked', true),
+        ('Melissa', 5, 'M', 'yellow', 'bitch', true),
+        ('Grinch', 6, 'F', 'green', 'fucked', true),
+        ('Wendy', 11, 'M', 'red', 'fucked', true),
+        (NULL, 1, 'F', 'red', 'fucked', true)              
